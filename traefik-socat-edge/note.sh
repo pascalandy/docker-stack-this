3abd3d0eeda46d3006a076131ee11109d07dcc6d
Pascal Andy <pascal@firepress.org>
2017-05-12 10:17:49 -0400

    traefik-socat-edge | update Q

--- traefik-socat-edge/note.sh
+++ traefik-socat-edge/note.sh
@@ -10,7 +10,20 @@ docker network ls
 docker stack deploy dockersocket -c dockersocket.yaml
 docker stack deploy traefik -c traefik.yaml
 
+docker stack deploy who1 -c who1.yml
+docker stack deploy who2 -c who2.yml
+docker stack deploy caddy1 -c caddy1.yml
+docker stack deploy nginx1 -c nginx1.yml
 
+
+# — — — # — — — # — — — #
+docker stack rm traefik
+docker stack rm dockersocket
+
+docker stack rm who1
+docker stack rm who2
+docker stack rm caddy1
+docker stack rm nginx1
 
 docker network rm ntw_front
 docker network rm ntw_socket
