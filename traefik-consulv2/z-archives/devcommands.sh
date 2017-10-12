0c1486d7d3641ecbd711f0fcc6fdd3fb386f91ab
Pascal Andy <pascal@firepress.org>
2017-10-11 20:58:15 -0400

    traefik-consulv2

---
+++ traefik-consulv2/z-archives/devcommands.sh
@@ -0,0 +1,19 @@
+docker swarm init --advertise-addr $(hostname -i); docker node ls;
+
+# Install common apps
+apk update && apk upgrade && apk add nano curl bash git wget unzip ca-certificates;
+
+# Clone repo
+cd /root;
+git clone https://github.com/pascalandy/docker-stack-this.git;
+cd docker-stack-this;
+git checkout 1.23;
+
+# Go to the actual project
+cd traefik-consulv2; echo; pwd; echo; ls -AlhF;
+
+# Make scripts executable
+chmod +x runup; chmod +x rundown; chmod +x runctop;
+
+# Run the stack
+./runup;
