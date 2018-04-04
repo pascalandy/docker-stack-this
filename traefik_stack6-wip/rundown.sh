f5b82426e5fee449be39c05fb10a68666996a98a
Pascal Andy <pascal@firepress.org>
2018-04-04 00:30:13 -0400

    Testing wordpress via traefik_stack6-wip

--- traefik_stack6-wip/rundown.sh
+++ traefik_stack6-wip/rundown.sh
@@ -16,7 +16,7 @@ docker stack rm toolweb || true; echo; sleep 1;
 
 docker stack rm toolmonitor || true; echo; sleep 1;
 
-# docker stack rm toolwp || true; echo; sleep 1;
+docker stack rm toolwp || true; echo; sleep 1;
 
 
 #docker stack rm proxpress || true; echo; sleep 1;
