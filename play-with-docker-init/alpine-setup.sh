6f3a60547d288500dba15223238a01ed2ba25c28
Pascal Andy <pascal@firepress.org>
2018-12-04 12:02:22 -0500

    minor comment udpates

--- play-with-docker-init/alpine-setup.sh
+++ play-with-docker-init/alpine-setup.sh
@@ -2,28 +2,28 @@
 
 # This script is made to run on a fresh play-with-docker node
 
-# install common apps
+# On play-with-docker, install common apps
 apk update && apk upgrade && apk add --no-cache                     \
     nano bash git curl wget unzip openssl tar tzdata                \
     ca-certificates                                                 && \
 
-# set local time
+# On play-with-docker, set local time
 cp /usr/share/zoneinfo/America/New_York /etc/localtime              && \
 echo "America/New_York" > /etc/timezone                             && \
 apk del tzdata                                                      && \
 
-# clean up
+# On play-with-docker, clean up
 rm -rf /var/cache/apk/* && sleep 1                                  && \
 
-# create Swarm manager
+# On play-with-docker, create Swarm manager
 docker swarm init --advertise-addr $(hostname -i)                   && \
 
-# Clone repo
+# On play-with-docker, clone repo
 echo                                                                && \
 cd /root                                                            && \
 git clone https://github.com/pascalandy/docker-stack-this.git       && \
 cd docker-stack-this                                                && \
 echo                                                                && \
-echo "The host is setup"                                            && \
+echo "Your play-with-docker setup is ready!"                        && \
 echo "Time to select a monorepo (ie traefik_stack5) "               && \
 echo                                                                ;
