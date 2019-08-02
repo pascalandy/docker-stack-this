badd1f186178a611d4ebfc65c192e3f0325da86c
Pascal Andy <pascal@firepress.org>
2019-08-02 16:00:34 -0400

    FIX script since I upgraded my hard drive
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- push_release.sh
+++ push_release.sh
@@ -1,37 +1,39 @@
 # This is specific to my local set up
 source .env .
 
-# Set env_var before running this script:
-# GITHUB_TOKEN="3122133122133211233211233211233211322313123"
-
 # vars
-#
-GIT_REPO="https://github.com/pascalandy/docker-stack-this"
-GOPATH=${HOME}/go
+# GITHUB_TOKEN="3122133122133211233211233211233211322313123"
+user="pascalandy"
+git_repo="docker-stack-this"
+local_repo="/Volumes/960G/_pascalandy/11_FirePress/Github/github_pascalandy/docker-stack-this"
 
 # The tag must be ready to push on git remote
 #
-cd ${HOME}/Documents/Github/github_pascalandy/docker-stack-this && \
-
+cd ${local_repo} && \
 git push --tags && \
 
 # Find the latest tag
 #
 tag_version="$(
-	git ls-remote --tags ${GIT_REPO} \
+	git ls-remote --tags ${git_repo} \
 		| cut -d$'\t' -f2 \
-		| grep -E '^refs/tags/[0-9]+\.[0-9]+' \
 		| cut -d/ -f3 \
-		| sort -rV \
-		| head -n1)" && \
+		| tail -n1)" && \
 echo ${tag_version} && \
 
 # Push release on GitHub like a boss
 # Requires: https://github.com/aktau/github-release
 #
 $GOPATH/bin/github-release release \
-  --user firepress-org \
-  --repo ${my_repo} \
+  --user ${user} \
+  --repo ${git_repo} \
   --tag ${tag_version} \
   --name ${tag_version} \
-  --description "Refer to [CHANGELOG.md](https://github.com/pascalandy/docker-stack-this/blob/master/CHANGELOG.md) for all details about this release."
+  --description "Refer to [CHANGELOG.md](https://github.com/pascalandy/docker-stack-this/blob/master/CHANGELOG.md) for details about this release."
+
+
+
+$GOPATH/bin/github-release info \
+  --user ${user} \
+  --repo ${git_repo} \
+  --tag ${tag_version}
