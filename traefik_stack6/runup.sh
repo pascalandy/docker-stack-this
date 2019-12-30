d700b0a6284a7b4d10c0e4fd48a8f06222a5de4b
Pascal Andy <pascal@firepress.org>
2019-12-30 17:33:27 -0500

    Update to Traefik V2 / DIR traefik_stack6 / squashed

--- traefik_stack6/runup.sh
+++ traefik_stack6/runup.sh
@@ -60,7 +60,6 @@ function parse_params() {
 # OUTS: None
 function main() {
     # load config and variables for this project
-    source "$(dirname "${BASH_SOURCE[0]}")/config_and_vars.sh"
     source "$(dirname "${BASH_SOURCE[0]}")/shellcheck.sh"
 
     trap script_trap_err ERR
@@ -83,68 +82,53 @@ function goto_myscript() {
     docker pull devmtl/figlet:1.0
     
     clear;
-    message_is="docker-stack-this"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && sleep 2 && echo;
+    message_is="docker"
+    docker run --rm devmtl/figlet:1.0 ${message_is} && \
+    message_is="stack-this"
+    docker run --rm devmtl/figlet:1.0 ${message_is} && sleep 2 && echo && \
 
+    # If existing, remove stacks: "
+    ./rundown.sh && \
+
+    # If not existing, create networks"
+    # create an overlay networks on docker swarm
+    arr=( "ntw_front" "ntw_proxy" )
+
+    for i in "${arr[@]}"; do
+      if [ ! "$(docker network ls --filter name=${i} -q)" ]; then
+      docker network create --driver overlay --attachable --opt encrypted "${i}"
+      echo "Network: ${i} was created."
+      else
+          echo "Network: ${i} already exist."
+      fi
+    done
+
+    message_is="Show networks" && docker run --rm devmtl/figlet:1.0 ${message_is} && echo && \
+    docker network ls | grep "ntw_" && echo && sleep 2 && \
     clear;
-    message_is="If existing, remove stacks: "
-    docker run --rm devmtl/figlet:1.0 ${message_is} && sleep 2 && echo;
-    ./rundown.sh
 
-    clear;
-    message_is="If not existing, create networks"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && echo;
-
-    this_net="ntw_front"
-        if [ ! "$(docker network ls --filter name=${this_net} -q)" ]; then
-            docker network create --driver overlay --attachable --opt encrypted "${this_net}"
-            echo "Network: ${this_net} was created."
-        else
-            echo "Network: ${this_net} already exist."
-        fi
-
-    this_net="ntw_proxy"
-        if [ ! "$(docker network ls --filter name=${this_net} -q)" ]; then
-            docker network create --driver overlay --attachable --opt encrypted "${this_net}"
-            echo "Network: ${this_net} was created."
-        else
-            echo "Network: ${this_net} already exist."
-        fi
-    echo;
-
-    message_is="Show networks"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && echo;
-
-    docker network ls | grep "ntw_" && echo && sleep 2;
-
-    clear;
-    message_is="Launch stacks"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && echo;
-
+    message_is="Launch stacks" && docker run --rm devmtl/figlet:1.0 ${message_is} && echo && \
 
     # Set ACME file
-    mkdir -pv ./configs && \
-    touch ./configs/acme.json && \
-    chmod 600 acme.json && \
+    #mkdir -pv           ~/./configs                   && \
+    #touch               ~/./configs/acme.json         && \
+    #cp dynamic_conf.yml ~/./configs/dynamic_conf.yml  && \
+    #cp traefik.yml      ~/./configs/traefik.yml       && \
+    #chmod 600 ~/./configs/acme.json         && \
+    #chmod 600 ~/./configs/dynamic_conf.yml  && \
+    #chmod 600 ~/./configs/traefik.yml       && \
 
-    cp dynamic_conf.yml ./configs/dynamic_conf.yml && \
-    cp traefik.yml ./configs/traefik.yml.yml && \
+    # deploy apps
+    docker stack deploy stkproxy -c stk_traefik.yml && \
+    docker stack deploy stkwebapp -c stk_web.yml && \
+    docker stack deploy stkgui -c stk_portainer.yml && echo && \
 
-    # traefik
-    docker stack deploy stkproxy -c stack-proxy.yml && echo; sleep 1 && \
-
-    # webapps
-    docker stack deploy stkwhoami1 -c stack-whoami1.yml && echo; sleep 1 && \
-
-    # gui
-    #docker stack deploy stkgui -c stack-portainer.yml && echo; sleep 1;
-
-    # swarmpit / constraint the db
+    # deploy swarmpit / constraint the db
     #export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
     #docker node update --label-add swarmpit.db-data=true $NODE_ID && echo;
     #docker stack deploy toolswarmpit -c stack-swarmpit.yml && echo; sleep 1;
 
-    # wordpress
+    # deploy wordpress
         # the system is path is at ./docker-stack5
     #_MYSQL_DIR="$(pwd)/html/db/mysql"
     #mkdir -p "$_MYSQL_DIR"
@@ -152,32 +136,42 @@ function goto_myscript() {
     #docker stack deploy toolwp -c toolwp.yml
     #echo; sleep 1;
 
-    clear;
-    message_is="Check services"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && echo;
+    FLAG_WAIT="true"
+    while [[ ${FLAG_WAIT} == "true" ]]; do
 
-    MIN="1"
-    MAX="8"
-    for ACTION in $(seq ${MIN} ${MAX}); do
-      echo && echo "docker service ls | Check ${ACTION}" of ${MAX}; echo;
-      docker service ls && echo && sleep 2;
-    done
-    echo;
+      # check if apps are running on K8s (or are in progress)
+      IN_PROGRESS=$(docker service ls | awk '{print $4}' | grep "0/" | wc -l)
 
-    docker stack ls && echo;
+      if [[ ${IN_PROGRESS} -ne 0 ]]; then
+        docker service ls ;
+        echo "===> Deployment in progress..." && echo && \
+        sleep 2 ;
+      elif [[ ${IN_PROGRESS} -eq 0 ]]; then
+        echo "App deployed!" && echo && \
+        FLAG_WAIT="false" && sleep 2 && \
+        docker service ls ;
+      else
+        echo "Unexpected error (err45)" ;
+      fi
+    done && \
+    echo && \
 
-    message_is="Your turn"
-    docker run --rm devmtl/figlet:1.0 ${message_is} && echo;
+
+    message_is="Check services" && docker run --rm devmtl/figlet:1.0 ${message_is} && echo && \
+    docker service ls && echo && \
+
+    message_is="Your turn" &&docker run --rm devmtl/figlet:1.0 ${message_is} && echo && \
 
     # See Traefik logs
     echo "If you enjoy this project, Give it a Star or Fork it :)";
-    echo "  https://github.com/pascalandy/docker-stack-this/" && echo;
+    echo "  https://github.com/pascalandy/docker-stack-this/" && echo && \
 
     # See Traefik logs
-    echo "Command ideas: ";
-    echo "  docker service logs -f stkproxy_traefik";
-    echo "  docker service ls";
-    echo "  docker stack ls"; echo;
+    echo "Command ideas: " && \
+    echo "  docker service logs -f stkproxy_traefik" && \
+    echo "  docker service ls" && \
+    echo "  docker ps" && \
+    echo "  docker stack ls" && echo;
 }
 
 # --- Entrypoint
